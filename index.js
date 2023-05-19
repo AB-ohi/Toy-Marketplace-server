6773b457e2628b73b2adfe39e5b0f81ecfe6d7a8
AB-ohi <askohi63907@gmail.com>
2023-05-19 15:19:35 +0600

    Q&A

--- index.js
+++ index.js
@@ -24,10 +24,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     const toyCollection = client.db('Toy-Marketplace').collection('toys');
+    const blogCollection = client.db('Toy-Marketplace').collection('blog');
+
     app.get('/toys', async(req,res)=>{
       const cursor = toyCollection.find();
+      const result = await cursor.toArray()
+      res.send(result);
+    })
+    app.get('/blog', async(req,res)=>{
+      const cursor = blogCollection.find();
       const result = await cursor.toArray()
       res.send(result);
     })
